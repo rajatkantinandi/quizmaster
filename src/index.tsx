c6d6cf5d1b31ddcca460022116dd037d611c90cd
Rajat Kanti Nandi <rajatkantinandi1993@gmail.com>
2022-04-12 19:37:18 +0530

    Feat: service worker - closes #13

--- src/index.tsx
+++ src/index.tsx
@@ -7,6 +7,7 @@ import './styles/spacing.css';
 import './styles/card.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 
 ReactDOM.render(
   <React.StrictMode>
@@ -19,3 +20,4 @@ ReactDOM.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+serviceWorkerRegistration.register();
