a563fba6f51986036ebf2a82a23e1737ce6b463d
Rajat Kanti Nandi <rajatkantinandi1993@gmail.com>
2022-04-12 20:10:22 +0530

    Feat: service worker (#22)
    * Feat: service worker
    - closes #13
    
    * Test pwa updates
    
    * Enable skipWaiting
    
    * revert minor change for testing
    
    * Add alert before reloading
    
    * Test update
    
    * Revert test code

--- src/index.tsx
+++ src/index.tsx
@@ -7,6 +7,7 @@ import './styles/spacing.css';
 import './styles/card.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 
 ReactDOM.render(
   <React.StrictMode>
@@ -19,3 +20,19 @@ ReactDOM.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+serviceWorkerRegistration.register({
+  onUpdate: (registration) => {
+    const waitingServiceWorker = registration.waiting;
+
+    if (waitingServiceWorker) {
+      waitingServiceWorker.addEventListener('statechange', (event: any) => {
+        if (event.target.state === 'activated') {
+          alert('The app has been updated 🎉! It will reload now!');
+          window.location.reload();
+        }
+      });
+      waitingServiceWorker.postMessage({ type: 'SKIP_WAITING' });
+    }
+  },
+});
