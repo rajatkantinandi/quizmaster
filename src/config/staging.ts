457819b1292ecf8b308c1dba03d9076e14cf022e
Rajat Kanti Nandi <rajatkantinandi1993@gmail.com>
2022-07-19 12:47:22 +0530

    Fix: urls again

--- src/config/staging.ts
+++ src/config/staging.ts
@@ -3,8 +3,8 @@ import { IConfig } from './types';
 const config: IConfig = {
   env: 'staging',
   productName: 'Quiz Master',
-  productUrl: 'https://quizmaster-staging.vercel.app/',
-  backendUrl: 'http://quizmaster-staging-dev.ap-south-1.elasticbeanstalk.com/',
+  productUrl: 'https://quizmaster-staging.vercel.app',
+  backendUrl: 'http://quizmaster-staging-dev.ap-south-1.elasticbeanstalk.com',
 };
 
 export default config;
