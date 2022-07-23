e328676ddba2af95b13c41fef07968753ee5a06e
govind007 <govind@activelylearn.com>
2022-07-23 12:59:16 +0530

    updating production url

--- src/config/production.ts
+++ src/config/production.ts
@@ -3,7 +3,7 @@ import { IConfig } from './types';
 const config: IConfig = {
   env: 'production',
   productName: 'Quiz Master',
-  productUrl: 'https://quizmaster.com',
+  productUrl: 'https://quizmaster.vercel.app',
   backendUrl: 'http://quizmaster-backend-dev.eba-9kt6svfh.ap-south-1.elasticbeanstalk.com',
 };
 
