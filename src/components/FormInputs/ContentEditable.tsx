4a9d2e43d068230b85a893317679cf8365f9cd65
Rajat Kanti Nandi <rajatkantinandi1993@gmail.com>
2023-06-11 14:01:56 +0530

    Add main buttons

--- src/components/FormInputs/ContentEditable.tsx
+++ src/components/FormInputs/ContentEditable.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { TextareaProps } from '@mantine/core';
 import { EditorContent, useEditor } from '@tiptap/react';
 import StarterKit from '@tiptap/starter-kit';
+import Link from '@tiptap/extension-link';
 import EditorToolbar from './EditorToolbar';
 
 type Props = TextareaProps & {
@@ -12,7 +13,20 @@ type Props = TextareaProps & {
 
 export default function ContentEditable({ label, autofocus, placeholder, value, onChange, ...rest }: Props) {
   const editor = useEditor({
-    extensions: [StarterKit],
+    extensions: [
+      StarterKit,
+      Link.configure({
+        openOnClick: false,
+        protocols: ['ftp', 'mailto', 'tel'],
+        autolink: true,
+        HTMLAttributes: {
+          // Allow search engines to follow links(remove nofollow)
+          rel: 'noopener noreferrer',
+          // Remove target entirely so links open in current tab
+          target: '_blank',
+        },
+      }),
+    ],
     content: value,
     enableCoreExtensions: true,
     autofocus: autofocus,
--- src/components/FormInputs/EditorToolbar.tsx
+++ src/components/FormInputs/EditorToolbar.tsx
@@ -1,9 +1,44 @@
 import { ChainedCommands, Editor } from '@tiptap/react';
 import React from 'react';
+import Icon from '../Icon';
 
 type Props = {
   editor: Editor | null;
 };
+
+const BUTTONS: {
+  commandName: keyof ChainedCommands;
+  children: React.ReactNode;
+  actionName: string;
+  actionParams?: Record<string, any>;
+}[] = [
+  { actionName: 'bold', commandName: 'toggleBold', children: <b>B</b> },
+  { actionName: 'italic', commandName: 'toggleItalic', children: <i>I</i> },
+  { actionName: 'strike', commandName: 'toggleStrike', children: <del>S</del> },
+  { actionName: 'code', commandName: 'toggleCode', children: <Icon name="code" width={18} height={18} /> },
+  { actionName: 'heading', commandName: 'toggleHeading', actionParams: { level: 3 }, children: 'H3' },
+  { actionName: 'heading', commandName: 'toggleHeading', actionParams: { level: 4 }, children: 'H4' },
+  {
+    actionName: 'bulletList',
+    commandName: 'toggleBulletList',
+    children: <Icon name="bulletList" width={18} height={18} />,
+  },
+  {
+    actionName: 'orderedList',
+    commandName: 'toggleOrderedList',
+    children: <Icon name="orderedList" width={18} height={18} />,
+  },
+  {
+    actionName: 'codeBlock',
+    commandName: 'toggleCodeBlock',
+    children: <Icon name="codeBlock" width={18} height={18} />,
+  },
+  {
+    actionName: 'blockquote',
+    commandName: 'toggleBlockquote',
+    children: <Icon name="blockquote" width={18} height={18} />,
+  },
+];
 
 export default function EditorToolbar({ editor }: Props) {
   if (!editor) {
@@ -12,17 +47,37 @@ export default function EditorToolbar({ editor }: Props) {
 
   return (
     <div className="editor-toolbar">
-      <EditorButton editor={editor} actionName="bold" commandName="toggleBold">
-        <b>B</b>
-      </EditorButton>
-      <EditorButton editor={editor} actionName="italic" commandName="toggleItalic">
-        <i>I</i>
-      </EditorButton>
-      <EditorButton editor={editor} actionName="strike" commandName="toggleStrike">
-        <del>S</del>
-      </EditorButton>
-      <EditorButton editor={editor} actionName="code" commandName="toggleCode">
-        {'<>'}
+      {BUTTONS.map((props) => (
+        <EditorButton editor={editor} {...props} key={props.commandName} />
+      ))}
+
+      <EditorButton
+        actionName="link"
+        editor={editor}
+        onPress={() => {
+          if (editor.isActive('link')) {
+            editor.chain().focus().unsetLink().run();
+            return;
+          }
+
+          const previousUrl = editor.getAttributes('link').href;
+          const url = window.prompt('Enter URL:', previousUrl || 'https://');
+
+          // cancelled
+          if (url === null) {
+            return;
+          }
+
+          // empty
+          if (url === '') {
+            editor.chain().focus().extendMarkRange('link').unsetLink().run();
+            return;
+          }
+
+          // update link
+          editor.chain().focus().extendMarkRange('link').setLink({ href: url }).run();
+        }}>
+        <Icon name="link" width={18} height={18} />
       </EditorButton>
     </div>
   );
@@ -30,19 +85,21 @@ export default function EditorToolbar({ editor }: Props) {
 
 type ButtonProps = {
   editor: Editor;
-  commandName: keyof ChainedCommands;
+  commandName?: keyof ChainedCommands;
   children: React.ReactNode;
   actionName: string;
+  actionParams?: Record<string, any>;
+  onPress?: () => void;
 };
 
-function EditorButton({ editor, children, commandName, actionName }: ButtonProps) {
+function EditorButton({ editor, children, commandName, actionName, actionParams, onPress }: ButtonProps) {
   return (
     <button
       type="button"
       title={actionName}
-      onClick={() => (editor.chain().focus()[commandName] as any)().run()}
-      disabled={!(editor.can().chain().focus() as any)[commandName]().run()}
-      className={editor.isActive(actionName) ? 'active' : ''}>
+      onClick={() => (commandName ? (editor.chain().focus()[commandName] as any)(actionParams).run() : onPress?.())}
+      disabled={commandName ? !(editor.can().chain().focus() as any)[commandName](actionParams).run() : false}
+      className={editor.isActive(actionName, actionParams) ? 'active' : ''}>
       {children}
     </button>
   );
