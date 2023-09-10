b9a638503c07e34bd81c823bae76f281ce54483a
govind007 <govind@activelylearn.com>
2023-09-10 16:57:33 +0530

    adding undo button

--- src/components/QuestionPlay/WithoutOptions.tsx
+++ src/components/QuestionPlay/WithoutOptions.tsx
@@ -1,7 +1,8 @@
-import React from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 import { Title, Button, Group, Box } from '@mantine/core';
 import { Option as IOption } from '../../types';
 import SanitizedHtml from '../SanitizedHtml';
+import UndoTimer from './undoTimer';
 
 interface Props {
   isAnswerRevealed: boolean;
@@ -20,12 +21,54 @@ export default function WithoutOptions({
   setIsAnswerRevealed,
   setSelectedChoices,
 }: Props) {
+  const [userResponse, setUserResponse] = useState({
+    isConfirmed: false,
+    value: null as any,
+  });
+  const [timer, setTimer] = useState(0);
+  const timerRef = useRef(null as any);
+
+  useEffect(() => {
+    if (timer > 0) {
+      if (timer === 5) {
+        clearTimeout(timerRef.current);
+        handleSelectedChoice(userResponse.value);
+        setUserResponse({
+          isConfirmed: false,
+          value: null,
+        });
+      } else {
+        timerRef.current = setTimeout(() => {
+          setTimer(timer + 1);
+        }, 1000);
+      }
+    }
+  }, [timer]);
+
   function handleSelectedChoice(choice: number[]) {
     setSelectedChoices(choice);
 
     setTimeout(() => {
       document.getElementById('btnSubmitResponse')?.click();
     }, 100);
+  }
+
+  function handleSubmitResponse(choice: number[]) {
+    setUserResponse({
+      isConfirmed: true,
+      value: choice,
+    });
+    setTimer(timer + 1);
+  }
+
+  function undoSubmit() {
+    clearTimeout(timerRef.current);
+    timerRef.current = null;
+    setTimer(0);
+    setUserResponse({
+      isConfirmed: false,
+      value: null,
+    });
   }
 
   const getQuestionTextStyles = (theme, isCorrect = false) =>
@@ -48,15 +91,28 @@ export default function WithoutOptions({
         sx={(theme) => getQuestionTextStyles(theme, !!options[0].text && options[0].isCorrect)}>
         <SanitizedHtml>{options[0].text}</SanitizedHtml>
       </Box>
-      {!isAttempted && (
+      {userResponse.isConfirmed ? (
         <Group>
-          <Button color="red" onClick={() => handleSelectedChoice([])}>
-            Incorrect
+          <Button variant="outline" onClick={() => handleSelectedChoice(userResponse.value)}>
+            Confirm Submit
           </Button>
-          <Button color="green" onClick={() => handleSelectedChoice([options[0].optionId])}>
-            Correct
+          <Button onClick={() => undoSubmit()} leftIcon={<UndoTimer>{5 - timer}</UndoTimer>}>
+            Undo
           </Button>
         </Group>
+      ) : (
+        <>
+          {!isAttempted && (
+            <Group>
+              <Button color="red" onClick={() => handleSubmitResponse([])}>
+                Incorrect
+              </Button>
+              <Button color="green" onClick={() => handleSubmitResponse([options[0].optionId])}>
+                Correct
+              </Button>
+            </Group>
+          )}
+        </>
       )}
     </>
   ) : (
---
+++ src/components/QuestionPlay/undoTimer.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import styles from './styles.module.css';
+
+export default function UndoTimer({ children }) {
+  return (
+    <>
+      <div className="pl-sm pt-sm">{children}</div>
+      <svg className={styles.svg}>
+        <circle r="18" cx="20" cy="20"></circle>
+      </svg>
+    </>
+  );
+}
