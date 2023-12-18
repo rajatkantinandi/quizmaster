366d8e5146cc9b271fbbc1979f9cd5896271b888
Rajat Kanti Nandi <rajatkantinandi1993@gmail.com>
2023-12-18 12:28:57 +0530

    Add device details to mixpanel events

--- src/helpers/track.ts
+++ src/helpers/track.ts
@@ -1,6 +1,7 @@
 import { TrackingEvent } from '../constants';
 import mixpanel from 'mixpanel-browser';
+import { getDeviceDetails } from './device';
 
 export const track = (eventName: TrackingEvent, data?: Record<string, any>) => {
-  mixpanel.track(eventName, data || {});
+  mixpanel.track(eventName, { ...getDeviceDetails(), ...(data || {}) });
 };
